d24368b73e3f0cc23e2340088c04e08baf1522b6
cmdr2 <secondary.cmdr2@gmail.com>
2026-06-26 15:54:43 +0530

    try nohup

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python bot.py > /dev/null 2> crash_log.txt &
+nohup python bot.py > /dev/null 2> crash_log.txt &
 disown
 
 echo "Bot launched. System errors will be saved to crash_log.txt"
